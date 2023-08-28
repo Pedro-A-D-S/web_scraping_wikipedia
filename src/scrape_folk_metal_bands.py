f6a57dc5243f6e0e608fd254f40109caf5bdffe3
Pedro <pedroalves0409@gmail.com>
2023-08-28 19:58:00 -0300

    refactor: :art: scrape_data_refactoring

--- src/scrape_folk_metal_bands.py
+++ src/scrape_folk_metal_bands.py
@@ -1,16 +1,28 @@
 import urllib.request
 import pandas as pd
+import yaml
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
+with open('../config/path.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
-url = 'https://en.wikipedia.org/wiki/List_of_folk_metal_bands'
+def get_data(url: str, data_path: str) -> pd.DataFrame:
+    '''
+    Scrapes data from a url and saves it to a csv file
 
-with urllib.request.urlopen(url) as i:
-        html = i.read()
+    Parameters:
+        url (str): The url to scrape
+        data_path (str): The path to save the csv file
+    returns:
+        None
+    '''
+    with urllib.request.urlopen(url) as i:
+            html = i.read()
+    data = pd.read_html(html)[0]
+    data.to_csv(data_path)
+    return None
 
-data = pd.read_html(html)[0]
-print(data)
-
-data.to_csv('folk_metal_bands.csv')
+if __name__ == '__main__':
+    
+    url = config['scrape']['url']
+    data_path = config['scrape']['save']
+    get_data(url, data_path)
